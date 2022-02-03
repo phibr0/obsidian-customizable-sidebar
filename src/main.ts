e1d85ab0edebb8ff2141830af1d00a39691ccb7e
Erin Schnabel <erinschnabel@gmail.com>
2022-02-02 21:01:58 -0500

    support float/pinning the sidebar on desktop

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
-import { App, Command, FuzzyMatch, FuzzySuggestModal, Modal, Notice, Plugin, PluginSettingTab, setIcon, Setting } from 'obsidian';
+import { Plugin, setIcon } from 'obsidian';
 import { addFeatherIcons } from './ui/icons';
 import CustomSidebarSettingsTab, { CustomSidebarSettings, DEFAULT_SETTINGS } from './ui/settingsTab';
 
+class PinSidebarSupport {
+	pinLeft?: HTMLElement;
+	pinRight?: HTMLElement;
+}
+
 export default class CustomSidebarPlugin extends Plugin {
 	settings: CustomSidebarSettings;
+	pinSidebarSupport?: PinSidebarSupport;
 	iconList: string[] = ["any-key", "audio-file", "blocks", "bold-glyph", "bracket-glyph", "broken-link", "bullet-list", "bullet-list-glyph", "calendar-with-checkmark", "check-in-circle", "check-small", "checkbox-glyph", "checkmark", "clock", "cloud", "code-glyph", "create-new", "cross", "cross-in-box", "crossed-star", "csv", "deleteColumn", "deleteRow", "dice", "document", "documents", "dot-network", "double-down-arrow-glyph", "double-up-arrow-glyph", "down-arrow-with-tail", "down-chevron-glyph", "enter", "exit-fullscreen", "expand-vertically", "filled-pin", "folder", "formula", "forward-arrow", "fullscreen", "gear", "go-to-file", "hashtag", "heading-glyph", "help", "highlight-glyph", "horizontal-split", "image-file", "image-glyph", "indent-glyph", "info", "insertColumn", "insertRow", "install", "italic-glyph", "keyboard-glyph", "languages", "left-arrow", "left-arrow-with-tail", "left-chevron-glyph", "lines-of-text", "link", "link-glyph", "logo-crystal", "magnifying-glass", "microphone", "microphone-filled", "minus-with-circle", "moveColumnLeft", "moveColumnRight", "moveRowDown", "moveRowUp", "note-glyph", "number-list-glyph", "open-vault", "pane-layout", "paper-plane", "paused", "pdf-file", "pencil", "percent-sign-glyph", "pin", "plus-with-circle", "popup-open", "presentation", "price-tag-glyph", "quote-glyph", "redo-glyph", "reset", "right-arrow", "right-arrow-with-tail", "right-chevron-glyph", "right-triangle", "run-command", "search", "sheets-in-box", "sortAsc", "sortDesc", "spreadsheet", "stacked-levels", "star", "star-list", "strikethrough-glyph", "switch", "sync", "sync-small", "tag-glyph", "three-horizontal-bars", "trash", "undo-glyph", "unindent-glyph", "up-and-down-arrows", "up-arrow-with-tail", "up-chevron-glyph", "uppercase-lowercase-a", "vault", "vertical-split", "vertical-three-dots", "wrench-screwdriver-glyph"];
 
 	async onload() {
-		console.log('loading plugin');
+		console.log('loading CustomSidebarPlugin %s', this.manifest.version);
 
 		await this.loadSettings();
 
@@ -24,7 +30,10 @@ export default class CustomSidebarPlugin extends Plugin {
 
 		this.addExtraCommands();
 
-		this.app.workspace.onLayoutReady(() => this.hideCommands());
+		this.app.workspace.onLayoutReady(() => {
+			this.hideCommands();
+			this.sidebarPins();
+		});
 	}
 
 	onunload() {
@@ -116,5 +125,140 @@ export default class CustomSidebarPlugin extends Plugin {
 				(children.item(i) as HTMLElement).style.display = "none";
 			}
 		}
+	}
+
+	sidebarPins() {
+		if (this.settings.pinSidebar === undefined && this.pinSidebarSupport) {
+			// clean up any sidebar pins & related classes if present
+
+			const leftParent = this.pinSidebarSupport.pinLeft.parentElement;
+			leftParent.removeChild(this.pinSidebarSupport.pinLeft);
+			delete this.pinSidebarSupport.pinLeft;
+
+			const rightParent = this.pinSidebarSupport.pinRight.parentElement;
+			rightParent.removeChild(this.pinSidebarSupport.pinRight);
+			delete this.pinSidebarSupport.pinRight;
+
+			this.getSidebarContainer(true).removeClass("is-pinned");
+			this.getSidebarContainer(true).removeClass("is-floating");
+
+			this.getSidebarContainer(false).removeClass("is-pinned");
+			this.getSidebarContainer(false).removeClass("is-floating");
+
+		} else if (this.settings.pinSidebar) {
+			// create sidebar pin buttons & related classes
+
+			if ( this.pinSidebarSupport === undefined ) {
+				// Commands are added once (not removed?)
+				this.addCommand({
+					id: "pin-left-sidebar",
+					name: "Toggle pin left Sidebar",
+					icon: "pin",
+					callback: async () => this.toggleLeftSidebar()
+				});
+
+				this.addCommand({
+					id: "pin-right-sidebar",
+					name: "Toggle pin right Sidebar",
+					icon: "pin",
+					callback: async () => this.toggleRightSidebar()
+				});
+			}
+
+			//@ts-ignore
+			const leftParent: HTMLElement = this.app.workspace.leftRibbon.containerEl;
+			let pinLeft = leftParent.children.namedItem("left-pin-sidebar") as HTMLElement;
+			if (pinLeft == null) {
+				pinLeft = leftParent.createDiv({
+					cls: "pin-sidebar pin-left-sidebar",
+					attr: {
+						name: "left-pin-sidebar"
+					}});
+				pinLeft.addEventListener("click", async (e) => {
+					this.toggleLeftSidebar();
+					return e.preventDefault();
+				});
+				leftParent.insertBefore(pinLeft, leftParent.firstChild);
+			}
+
+			//@ts-ignore
+			const rightParent: HTMLElement = this.app.workspace.rightRibbon.containerEl;
+			let pinRight = rightParent.children.namedItem("right-pin-sidebar") as HTMLElement;
+			if ( pinRight == null ) {
+				pinRight = rightParent.createDiv({
+					cls: "pin-sidebar pin-right-sidebar",
+					attr: {
+						name: "right-pin-sidebar",
+					}});
+				pinRight.addEventListener("click", async (e) => {
+					this.toggleRightSidebar();
+					return e.preventDefault();
+				});
+				rightParent.insertBefore(pinRight, rightParent.firstChild);
+			}
+
+			// Save elements for reference/toggle
+			this.pinSidebarSupport = {
+				pinLeft,
+				pinRight
+			};
+
+			this.setAttributes(pinLeft,
+					this.getSidebarContainer(true),
+					this.settings.pinSidebar.left === undefined ? true : this.settings.pinSidebar.left);
+
+			this.setAttributes(pinRight,
+					this.getSidebarContainer(false),
+					this.settings.pinSidebar.right === undefined ? true : this.settings.pinSidebar.right);
+		}
+	}
+
+	async toggleLeftSidebar() {
+		if ( this.settings.pinSidebar && this.pinSidebarSupport ) {
+			this.settings.pinSidebar.left = ! this.settings.pinSidebar.left;
+			this.setAttributes(this.pinSidebarSupport.pinLeft,
+				this.getSidebarContainer(true),
+				this.settings.pinSidebar.left);
+			await this.saveSettings();
+		}
+	}
+
+	async toggleRightSidebar() {
+		if ( this.settings.pinSidebar && this.pinSidebarSupport ) {
+			this.settings.pinSidebar.right = ! this.settings.pinSidebar.right;
+			this.setAttributes(this.pinSidebarSupport.pinRight,
+				this.getSidebarContainer(false),
+				this.settings.pinSidebar.right);
+			await this.saveSettings();
+		}
+	}
+
+	setAttributes(pin: HTMLElement, sidebar: HTMLElement, value: boolean) {
+		if ( value ) {
+			setIcon(pin, "filled-pin");
+
+			pin.addClass("is-pinned");
+			sidebar.addClass("is-pinned");
+
+			pin.removeClass("is-floating");
+			sidebar.removeClass("is-floating");
+		} else {
+			setIcon(pin, "pin");
+
+			pin.removeClass("is-pinned");
+			sidebar.removeClass("is-pinned");
+
+			pin.addClass("is-floating");
+			sidebar.addClass("is-floating");
+		}
+	}
+
+	getSidebarContainer(leftSplit: boolean) {
+		if ( leftSplit ) {
+			//@ts-ignore
+			return this.app.workspace.leftSplit.containerEl;
+		}
+		//@ts-ignore
+		return this.app.workspace.rightSplit.containerEl;
 	}
 }
--- src/ui/settingsTab.ts
+++ src/ui/settingsTab.ts
@@ -4,144 +4,168 @@ import CommandSuggester from "./commandSuggester";
 import IconPicker from "./iconPicker";
 
 export interface CustomSidebarSettings {
-    sidebarCommands: Command[];
-    hiddenCommands: string[];
+	sidebarCommands: Command[];
+	hiddenCommands: string[];
+	pinSidebar?: {
+		left?: boolean;
+		right?: boolean;
+	}
 }
 
 export const DEFAULT_SETTINGS: CustomSidebarSettings = {
-    sidebarCommands: [],
-    hiddenCommands: [],
+	sidebarCommands: [],
+	hiddenCommands: [],
 }
 
 export default class CustomSidebarSettingsTab extends PluginSettingTab {
-    plugin: CustomSidebarPlugin;
+	plugin: CustomSidebarPlugin;
 
-    constructor(app: App, plugin: CustomSidebarPlugin) {
-        super(app, plugin);
-        this.plugin = plugin;
-        addEventListener("CS-addedCommand", () => {
-            this.display();
-        });
-    }
+	constructor(app: App, plugin: CustomSidebarPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+		addEventListener("CS-addedCommand", () => {
+			this.display();
+		});
+	}
 
-    display(): void {
-        let { containerEl } = this;
+	display(): void {
+		let { containerEl } = this;
 
-        containerEl.empty();
+		containerEl.empty();
 
-        containerEl.createEl('h2', { text: 'Customizable Sidebar Settings' });
+		containerEl.createEl('h2', { text: 'Customizable Sidebar Settings' });
 
-        new Setting(containerEl)
-            .setName("Add Command to Sidebar")
-            .setDesc("Add a new command to the left Sidebar Ribbon")
-            .addButton((bt) => {
-                bt.setButtonText("Add Command")
-                    .onClick(() => {
-                        new CommandSuggester(this.plugin).open();
-                    });
-            });
+		new Setting(containerEl)
+			.setName("Add Command to Sidebar")
+			.setDesc("Add a new command to the left Sidebar Ribbon")
+			.addButton((bt) => {
+				bt.setButtonText("Add Command")
+					.onClick(() => {
+						new CommandSuggester(this.plugin).open();
+					});
+			});
 
-        this.plugin.settings.sidebarCommands.forEach((c, index) => {
-            const iconDiv = createDiv({ cls: "CS-settings-icon" });
-            setIcon(iconDiv, c.icon, 20);
-            const setting = new Setting(containerEl)
-                .setName(c.name);
+		this.plugin.settings.sidebarCommands.forEach((c, index) => {
+			const iconDiv = createDiv({ cls: "CS-settings-icon" });
+			setIcon(iconDiv, c.icon, 20);
+			const setting = new Setting(containerEl)
+				.setName(c.name);
 
-            if (index > 0) {
-                setting.addExtraButton(bt => {
-                    bt.setIcon("up-arrow-with-tail")
-                        .setTooltip("Move up")
-                        .onClick(async () => await this.moveCommand(c, 'up'));
-                })
-            }
-            if (index < this.plugin.settings.sidebarCommands.length - 1) {
-                setting.addExtraButton(bt => {
-                    bt.setIcon("down-arrow-with-tail")
-                        .setTooltip("Move down")
-                        .onClick(async () => await this.moveCommand(c, 'down'));
-                })
-            }
-            setting
-                .addExtraButton(bt => {
-                    bt.setIcon("trash")
-                        .setTooltip("Remove Command")
-                        .onClick(async () => {
-                            this.plugin.settings.sidebarCommands.remove(c);
-                            await this.plugin.saveSettings();
-                            this.display();
-                            const ribbonButton = Array.from(this.leftRibbonPanel.children)
-                                .find((btn) => c.name === btn.getAttribute('aria-label'))
-                            this.leftRibbonPanel.removeChild(ribbonButton);
-                        })
-                })
-                .addExtraButton(bt => {
-                    bt.setIcon("gear")
-                        .setTooltip("Edit Icon")
-                        .onClick(() => {
-                            new IconPicker(this.plugin, c, true).open();
-                        })
-                });
-            setting.nameEl.prepend(iconDiv);
-            setting.nameEl.addClass("CS-flex");
-        });
+			if (index > 0) {
+				setting.addExtraButton(bt => {
+					bt.setIcon("up-arrow-with-tail")
+						.setTooltip("Move up")
+						.onClick(async () => await this.moveCommand(c, 'up'));
+				})
+			}
+			if (index < this.plugin.settings.sidebarCommands.length - 1) {
+				setting.addExtraButton(bt => {
+					bt.setIcon("down-arrow-with-tail")
+						.setTooltip("Move down")
+						.onClick(async () => await this.moveCommand(c, 'down'));
+				})
+			}
+			setting
+				.addExtraButton(bt => {
+					bt.setIcon("trash")
+						.setTooltip("Remove Command")
+						.onClick(async () => {
+							this.plugin.settings.sidebarCommands.remove(c);
+							await this.plugin.saveSettings();
+							this.display();
+							const ribbonButton = Array.from(this.leftRibbonPanel.children)
+								.find((btn) => c.name === btn.getAttribute('aria-label'))
+							this.leftRibbonPanel.removeChild(ribbonButton);
+						})
+				})
+				.addExtraButton(bt => {
+					bt.setIcon("gear")
+						.setTooltip("Edit Icon")
+						.onClick(() => {
+							new IconPicker(this.plugin, c, true).open();
+						})
+				});
+			setting.nameEl.prepend(iconDiv);
+			setting.nameEl.addClass("CS-flex");
+		});
 
-        //@ts-ignore
-        const children: HTMLCollection = this.app.workspace.leftRibbon.ribbonActionsEl.children;
-        for (let i = 0; i < children.length; i++) {
-            if (!this.plugin.settings.sidebarCommands.contains(this.plugin.settings.sidebarCommands.find(c => c.name === (children.item(i) as HTMLElement).getAttribute("aria-label")))) {
-                new Setting(containerEl)
-                    .setName("Hide " + (children.item(i) as HTMLElement).getAttribute("aria-label") + "?")
-                    .addToggle(cb => {
-                        cb.setValue((children.item(i) as HTMLElement).style.display === "none")
-                        cb.onChange(async value => {
-                            if(value === true) {
-                                (children.item(i) as HTMLElement).style.display = "none";
-                                this.plugin.settings.hiddenCommands.push((children.item(i) as HTMLElement).getAttribute("aria-label"));
-                                await this.plugin.saveSettings();
-                            } else {
-                                (children.item(i) as HTMLElement).style.display = "flex";
-                                this.plugin.settings.hiddenCommands.remove((children.item(i) as HTMLElement).getAttribute("aria-label"));
-                                await this.plugin.saveSettings();
-                            }
-                        });
-                    });
-            }
-        }
+		//@ts-ignore
+		const children: HTMLCollection = this.app.workspace.leftRibbon.ribbonActionsEl.children;
+		for (let i = 0; i < children.length; i++) {
+			if (!this.plugin.settings.sidebarCommands.contains(this.plugin.settings.sidebarCommands.find(c => c.name === (children.item(i) as HTMLElement).getAttribute("aria-label")))) {
+				new Setting(containerEl)
+					.setName("Hide " + (children.item(i) as HTMLElement).getAttribute("aria-label") + "?")
+					.addToggle(cb => {
+						cb.setValue((children.item(i) as HTMLElement).style.display === "none")
+						cb.onChange(async value => {
+							if(value === true) {
+								(children.item(i) as HTMLElement).style.display = "none";
+								this.plugin.settings.hiddenCommands.push((children.item(i) as HTMLElement).getAttribute("aria-label"));
+								await this.plugin.saveSettings();
+							} else {
+								(children.item(i) as HTMLElement).style.display = "flex";
+								this.plugin.settings.hiddenCommands.remove((children.item(i) as HTMLElement).getAttribute("aria-label"));
+								await this.plugin.saveSettings();
+							}
+						});
+					});
+			}
+		}
 
-        new Setting(containerEl)
-            .setName('Donate')
-            .setDesc('If you like this plugin, consider donating to support continued development:')
-            .setClass("AT-extra")
-            .addButton((bt) => {
-                bt.buttonEl.outerHTML = `<a href="https://www.buymeacoffee.com/phibr0"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=phibr0&button_colour=5F7FFF&font_colour=ffffff&font_family=Inter&outline_colour=000000&coffee_colour=FFDD00"></a>`;
-            });
+		new Setting(containerEl)
+			.setName("Desktop only: Support floating sidebars")
+			.setDesc("Change the sidebar behavior to match mobile: they will open above content panes unless pinned.")
+			.addToggle(toggle => {
+				toggle.setValue(this.plugin.settings.pinSidebar !== undefined);
+				toggle.onChange(async value => {
+					if ( value ) {
+						// start with default pinned/open
+						this.plugin.settings.pinSidebar = {
+							right: true,
+							left: true
+						};
+					} else {
+						delete this.plugin.settings.pinSidebar;
+					}
+					this.plugin.sidebarPins();
+					await this.plugin.saveSettings();
+				});
+			});
 
-    }
+		new Setting(containerEl)
+			.setName('Donate')
+			.setDesc('If you like this plugin, consider donating to support continued development:')
+			.setClass("AT-extra")
+			.addButton((bt) => {
+				bt.buttonEl.outerHTML = `<a href="https://www.buymeacoffee.com/phibr0"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=phibr0&button_colour=5F7FFF&font_colour=ffffff&font_family=Inter&outline_colour=000000&coffee_colour=FFDD00"></a>`;
+			});
 
-    private get leftRibbonPanel(): HTMLElement {
-        // @ts-ignore `ribbonActionsEl` is not defined in api
-        return this.app.workspace.leftRibbon.ribbonActionsEl as HTMLElement;
-    }
+	}
 
-    private async moveCommand(command: Command, direction: 'up' | 'down') {
-        // Move command in settings
-        const commands = this.plugin.settings.sidebarCommands;
-        const index = commands.indexOf(command);
-        if (direction === 'up') {
-            commands.splice(index - 1, 0, commands.splice(index, 1)[0]);
-        } else {
-            commands.splice(index + 1, 0, commands.splice(index, 1)[0]);
-        }
-        await this.plugin.saveSettings();
-        this.display();
+	private get leftRibbonPanel(): HTMLElement {
+		// @ts-ignore `ribbonActionsEl` is not defined in api
+		return this.app.workspace.leftRibbon.ribbonActionsEl as HTMLElement;
+	}
 
-        // Move command button in left ribbon
-        const ribbonButton = Array.from(this.leftRibbonPanel.children)
-            .find((btn) => command.name === btn.getAttribute('aria-label'));
-        if (direction === 'up') {
-            ribbonButton.previousSibling.before(ribbonButton);
-        } else {
-            ribbonButton.nextSibling.after(ribbonButton);
-        }
-    }
+	private async moveCommand(command: Command, direction: 'up' | 'down') {
+		// Move command in settings
+		const commands = this.plugin.settings.sidebarCommands;
+		const index = commands.indexOf(command);
+		if (direction === 'up') {
+			commands.splice(index - 1, 0, commands.splice(index, 1)[0]);
+		} else {
+			commands.splice(index + 1, 0, commands.splice(index, 1)[0]);
+		}
+		await this.plugin.saveSettings();
+		this.display();
+
+		// Move command button in left ribbon
+		const ribbonButton = Array.from(this.leftRibbonPanel.children)
+			.find((btn) => command.name === btn.getAttribute('aria-label'));
+		if (direction === 'up') {
+			ribbonButton.previousSibling.before(ribbonButton);
+		} else {
+			ribbonButton.nextSibling.after(ribbonButton);
+		}
+	}
 }
